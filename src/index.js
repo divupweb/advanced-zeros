be031f22b34a32bd5512bb52f3ebf4a71ce3a193
divupweb <divupwebgame@gmail.com>
2019-03-13 20:05:52 +0300

    feat: initial start task code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+var k = 1;
+var str="";
+var number =100;
+var base = 8;
+  if(number<base)
+     str = ""+number;
+     else{
+             celoe = Math.floor(number/base);
+             ost = number - celoe*base;
+             str+=ost;
+             while(celoe>base){
+                 celoe = Math.floor(number/base);
+                 ost = celoe - celoe*base;
+                 str+=celoe;
+
+             }
+             
+      }
+
+  console.log("Answer:"+str.split("").reverse().join(""));
+
+
+
+
+
+function rec(k){
+     if(param<Math.pow(5,k))
+      return 0;
+      return Math.floor(param/Math.pow(5,k))+rec(k+1);
+
+}
+
+var result = rec(k);
+
+console.log("answer: "+result);
+
+return result
 }
